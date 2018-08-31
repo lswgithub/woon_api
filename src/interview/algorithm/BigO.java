c3dbcf85a5323d1508ef0bdf0f234481d78df9c1
Sowoon Lee <sowoon123@gmail.com>
2018-08-31 08:49:41 +0000

    init algorithm

---
+++ src/interview/algorithm/BigO.java
@@ -0,0 +1,6 @@
+//Describe the data transfer "algorithm" runtime as
+//Electronic Transfer :O(s), where s is the size of the file. this means that the time to trasfer the file increases linearly with the size of the file.
+//O(log N),O(N log N),O(N),)(N2) and O(2N)
+//-O(big O)big O describes an upper bound on the time
+//-Ω(big omega): Ω is the equivalent concept but for lower bound.
+//-θ(big theta): Θ means both O and Θ
