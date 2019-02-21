70e9218efae8d372078df8359dc6b2e2bb1021a1
Sowoon Lee <sowoon123@gmail.com>
2019-02-21 07:33:46 +0000

    js..

--- src_js/commonUtil.js
+++ src_js/commonUtil.js
@@ -47,3 +47,55 @@ function getDate(dateObj, format) {
 function getEndOfMonthDay(m, y) { 
     return m===2?y&3||!(y%25)&&y&15?28:29:30+(m+(m>>3)&1);
 }
+
+/**
+	 * 한글 조사 연결 (을/를,이/가,은/는,로/으로)
+	 * 1. 종성에 받침이 있는 경우 '을/이/은/으로/과'
+	 * 2. 종성에 받침이 없는 경우 '를/가/는/로/와'
+	 * 3. '로/으로'의 경우 종성의 받침이 'ㄹ' 인경우 '로'
+	 * 참고 1 : http://gun0912.tistory.com/65 (소스 참고)
+	 * 참고 2 : http://www.klgoodnews.org/board/bbs/board.php?bo_table=korean&wr_id=247 (조사 원리 참고)
+     * 출처 : https://opensrc.tistory.com/176
+	 * @param name
+	 * @param firstValue
+	 * @param secondValue
+	 * @return
+	 */
+	function getPostWord(str,firstVal, secondVal) {
+
+	try {
+			var laststr = str.charAt(str.length() - 1);
+			// 한글의 제일 처음과 끝의 범위밖일 경우는 오류
+			if (laststr < 0xAC00 || laststr > 0xD7A3) {
+			    return str;
+			}
+	
+			var lastCharIndex = (laststr - 0xAC00) % 28;
+	
+			// 종성인덱스가 0이상일 경우는 받침이 있는경우이며 그렇지 않은경우는 받침이 없는 경우
+			if(lastCharIndex > 0) {
+				// 받침이 있는경우
+				// 조사가 '로'인경우 'ㄹ'받침으로 끝나는 경우는 '로' 나머지 경우는 '으로'
+				if(firstVal.equals("으로") && lastCharIndex == 8) {
+					str += secondVal;
+				} else {
+					str += firstVal;
+				}
+			} else {
+				// 받침이 없는 경우
+				str += secondVal;
+			}
+		} catch (e) {
+			//e.printStackTrace();
+		}
+
+		return str;
+	}
+
+var name;
+name = "네이버";
+console.log(getPostWord(name,"으로","로"));
+console.log(getPostWord(name,"을","를"));
+console.log(getPostWord(name,"이","가"));
+console.log(getPostWord(name,"은","는"));
+console.log(getPostWord(name,"과","와"));
